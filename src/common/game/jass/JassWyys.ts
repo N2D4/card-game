326d25040c383d89c0c27d2e1ceddd44d4d14c21
jannikg <jannikg@student.ethz.ch>
2018-11-18 23:36:00 +0100

    created a JassWyys class containing scoring + comparing of a Wyys

---
+++ src/common/game/jass/JassWyys.ts
@@ -0,0 +1,42 @@
+import {JassCard, JassColor, JassType} from './JassCard';
+
+
+export class JassWyys {
+    public constructor(public readonly card: JassCard, public readonly type: JassWyysType) {
+       
+    }
+
+    public static compare(a: JassWyys, b: JassWyys): number {
+        if (a.getScore() === b.getScore()) {
+            if (a.type === JassWyysType.FUENFBLATT && b.type === JassWyysType.VIERGLEICHE)
+                return 1;
+            if (a.type === JassWyysType.VIERGLEICHE && b.type === JassWyysType.FUENFBLATT)
+                return -1;
+            else
+                return 0;
+        } else 
+            return a.getScore() - b.getScore();
+    }
+
+    public getScore(): number {
+        switch (this.type) {
+            case JassWyysType.DREIBLATT:
+                return 20;
+            case JassWyysType.VIERBLATT:
+                return 50;
+            case JassWyysType.VIERGLEICHE:
+                return this.card.type === JassType.NEUNE ? 150
+                     : this.card.type === JassType.UNDER ? 200
+                     : 100;
+            case JassWyysType.FUENFBLATT:
+                return 100;
+            default:
+                return 0;
+        }
+    }
+
+}
+
+export enum JassWyysType {
+    DREIBLATT, VIERBLATT, FUENFBLATT, VIERGLEICHE,
+}
