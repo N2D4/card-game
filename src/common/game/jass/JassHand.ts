54cd119ca5ecb9631127974aacfefcc6c0902460
jannikg <jannikg@student.ethz.ch>
2018-11-18 23:35:01 +0100

    added getWyys Function

--- src/common/game/jass/JassHand.ts
+++ src/common/game/jass/JassHand.ts
@@ -1,5 +1,6 @@
 import {JassCard, JassColor, JassType} from './JassCard';
 import JassStich from './JassStich';
+import {JassWyys, JassWyysType} from './JassWyys';
 
 export default class JassHand {
     public constructor(public readonly cards: JassCard[]) {
@@ -20,5 +21,40 @@ export default class JassHand {
 
     public contains(card: JassCard): boolean {
         return this.cards.indexOf(card) >= 0;
+    }
+
+    public getWyysOptions(): JassWyys[] {
+        const cards: JassCard[] = [...this.cards];
+        cards.sort(JassCard.lexicographicCompare);
+
+        const wyys: JassWyys[] = [];
+        const count: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+        let l = 1;
+        for (let i = 1; i < cards.length; i++) {
+            // find sequence of same color
+            if (cards[i].color === cards[i - 1].color && cards[i].type === cards[i - 1].type + 1)
+                l++;
+            else 
+                l = 1;
+            
+            // add 3,4 or 5 Blatt
+            if (l === 3)
+                wyys.push(new JassWyys(cards[i], JassWyysType.DREIBLATT));
+            if (l === 4)
+                wyys.push(new JassWyys(cards[i], JassWyysType.VIERBLATT));
+            if (l === 5)
+                wyys.push(new JassWyys(cards[i], JassWyysType.FUENFBLATT));
+            
+            // increase count of same card
+            count[cards[i].type - 6]++;
+        }
+
+        for (let i = 0; i < 9; i++) {
+            if (count[i] === 4)
+                wyys.push(new JassWyys(JassCard.getCard(JassColor.EICHEL, i + 6), JassWyysType.VIERGLEICHE));
+        }
+
+        return wyys;
     }
 }
