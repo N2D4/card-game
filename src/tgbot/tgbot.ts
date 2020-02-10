2d47e24eb3c11d3a687dcfbb138dc2da0b5d1d3a
Stan Wohlwend <n2d4xc@gmail.com>
2020-02-10 14:24:42 +0100

    Add /stop command

--- src/tgbot/tgbot.ts
+++ src/tgbot/tgbot.ts
@@ -4,22 +4,35 @@ export function startBot() {
     const token = process.env.TG_API_KEY || 'not set';
     const gameName = process.env.TG_GAME_NAME || 'not set';
     const gameURL = process.env.TG_GAME_URL || 'not set';
+    const admins = new Set((process.env.TG_ADMIN_IDS || '').split(',').map(a => Number(a)));
 
-    // Create a bot that uses 'polling' to fetch new updates
     const bot = new TelegramBot(token, {polling: true});
 
-    // Matches "/echo [whatever]"
     bot.onText(/\/play/, (msg) => {
-        // send back the matched "whatever" to the chat
         bot.sendGame(msg.chat.id, gameName);
+    });
+
+    bot.onText(/\/myid/, (msg) => {
+        bot.sendMessage(msg.chat.id, `Your ID: ${msg.from?.id}`);
+    });
+
+    bot.onText(/\/stop/, (msg) => {
+        const fromID = msg.from?.id;
+        if (fromID !== undefined && admins.has(fromID)) {
+            bot.sendMessage(msg.chat.id, `Stopping the server.`);
+            console.log(`Admin with id ${msg.from?.id} stopped the server!`);
+            process.exit();
+        } else {
+            bot.sendMessage(msg.chat.id, `You don't have permissions to do this!`);
+        }
     });
 
     bot.on('callback_query', (callbackQuery) => {
         bot.answerCallbackQuery(callbackQuery.id, { url: gameURL });
     });
 
-    bot.on('inline_query', (callbackQuery) => {
-        bot.answerInlineQuery(callbackQuery.id, [{
+    bot.on('inline_query', (inlineQuery) => {
+        bot.answerInlineQuery(inlineQuery.id, [{
             type: 'game',
             id: gameName,
             game_short_name: gameName
